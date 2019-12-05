a2c744c9d5ca36efc8cf06df387abce2b4af0b2e
channchetra <channchetra@gmail.com>
2019-12-05 18:16:33 +0700

    Add Block

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html class="responsive" {!! get_language_attributes() !!}>
+  @include('partials.head')
+  <body>
+    @php do_action('get_header') @endphp
+    <div class="wrapper">
+        @include('partials.header-mobile')
+        @include('partials.header')
+        @include('partials.marquee')
+        <div class="content">
+          <div class="container plr-lg-30">
+            <div class="row">
+              <div class="col-lg-8">
+                <div class="detail-wrap">
+                  <main class="main">
+                    @yield('content')
+                  </main>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--End of Body Class Content -->
+    @php do_action('get_footer') @endphp
+        <div class="content">
+          @include('partials.footer')
+          @include('partials.responsive-switcher')
+        </div>
+    </div>
+    <!--End of Wrapper -->
+    @php wp_footer() @endphp
+  </body>
+</html>
