9d76b8ed1c146acb30b4f6ac9ac3aa60181f32ac
channchetra <channchetra@gmail.com>
2020-01-06 17:10:12 +0700

    Fixed singular page

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -1,1 +1,6 @@
 @extends('layouts.home')
+@section('content')
+  @while(have_posts()) @php the_post() @endphp
+    @include('partials.content-page')
+  @endwhile
+@endsection
--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -4,23 +4,13 @@
   <body>
     @php do_action('get_header') @endphp
     <div class="wrapper">
-        @include('partials.header-mobile')
-        @include('partials.header')
-        @include('partials.marquee')
-        <div class="content">
-          <div class="container plr-lg-30">
-            <div class="row">
-              <div class="col-lg-8">
-                <div class="detail-wrap">
-                  <main class="main">
-                    @yield('content')
-                  </main>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <!--End of Body Class Content -->
+      @include('partials.header-mobile')
+      @include('partials.header')
+      @include('partials.marquee')
+      <div class="content">
+        @yield('content')
+      </div>
+    <!--End of Body Class Content -->
     @php do_action('get_footer') @endphp
         <div class="content">
           @include('partials.footer')
